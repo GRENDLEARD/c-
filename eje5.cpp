3bee6ea765c263cdbb47ef9c6ae6b6e474a3f3f0
GRENDLEARD <134238909+GRENDLEARD@users.noreply.github.com>
2023-08-18 12:49:20 -0500

    Update eje5.cpp

--- eje5.cpp
+++ eje5.cpp
@@ -1,22 +1,22 @@
 #include <iostream>
-#include <cmath> // Incluir la librería para funciones matemáticas
+#include <cmath> 
 
 using namespace std;
 
 int main() {
     double cateto1, cateto2, hipotenusa;
 
-    // Solicitar los valores de los catetos al usuario
+   
     cout << "Ingresa el valor del primer cateto: ";
     cin >> cateto1;
 
     cout << "Ingresa el valor del segundo cateto: ";
     cin >> cateto2;
 
-    // Calcular la hipotenusa usando el teorema de Pitágoras
+   
     hipotenusa = sqrt(cateto1 * cateto1 + cateto2 * cateto2);
 
-    // Mostrar el resultado
+    
     cout << "La longitud de la hipotenusa es: " << hipotenusa << endl;
 
     return 0;
