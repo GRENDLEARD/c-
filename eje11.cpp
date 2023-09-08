495e58cbd6676f147bb40ddcc361b6d62b333ca8
GRENDLEARD <134238909+GRENDLEARD@users.noreply.github.com>
2023-09-07 23:38:42 -0500

    Update eje11.cpp

--- eje11.cpp
+++ eje11.cpp
@@ -1,1 +1,34 @@
+#include <iostream>
 
+using namespace std;
+
+int main() {
+    int num1, num2;
+
+    cout << "Ingrese el primer numero: ";
+    cin >> num1;
+
+    cout << "Ingrese el segundo numero: ";
+    cin >> num2;
+
+    int minimo; // 5
+
+    if (num1 < num2) { // 5 y 10
+        minimo = num1;
+    } else {
+        minimo = num2;
+    }
+
+    int mcd = 0;
+
+        for (int i = 1; i <= minimo; i++) {
+            if (num1 % i == 0 && num2 % i == 0) {
+                mcd = i;
+            }
+        }
+        
+
+    cout << "El MCD de " << num1 << " y " << num2 << " es: " << mcd << endl;
+
+    return 0;
+}
