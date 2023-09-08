cc43c88e1a1332e4ce056732d55106ea0436cc8e
GRENDLEARD <134238909+GRENDLEARD@users.noreply.github.com>
2023-09-07 23:37:30 -0500

    Update eje10.cpp

--- eje10.cpp
+++ eje10.cpp
@@ -1,1 +1,17 @@
+#include <iostream>
 
+using namespace std;
+
+int main() {
+    int suma = 0;
+
+    for (int i = 4; i <= 400; i+=4 ) {
+        cout << i << " ";
+        suma = suma + i;
+
+    }
+
+    cout << "la suma de la serie es: " << suma << endl ;
+
+    return 0;
+}
