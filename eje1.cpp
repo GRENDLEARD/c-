ae492fc114aa1f3eeb2c96e5d2563a9005ce17dd
GRENDLEARD <134238909+GRENDLEARD@users.noreply.github.com>
2023-09-07 23:10:40 -0500

    Update eje1.cpp

--- eje1.cpp
+++ eje1.cpp
@@ -1,21 +1,35 @@
 #include <iostream>
-#include <cmath> 
 
 using namespace std;
 
-int main() {
-    const double PI = 3.14159;
-    double radio, perimetro, area;
 
-    cout << "Ingrese el radio del circulo: ";
-        cin >> radio;
+int main()
+{   
+    int numero1 , numero2, numero3, numero4;
 
-        perimetro = 2 * PI * radio;
-            area = PI * pow(radio, 2); 
+    cout << "ingrese el primer numero: ";
+    cin >> numero1;
+
+    cout << "ingrese el segundo numero: ";
+    cin >> numero2;
+    cout << "ingrese el tercero numero: ";
+    cin >> numero3;
+    cout << "ingrese cuarto numero: ";
+    cin >> numero4;
+
+    if (numero1 > numero2 && numero1 > numero3 && numero1 > numero4){
+        cout<< "el numero mayor es: " << numero1;
+    } else if(numero2 > numero1 && numero2 > numero3 && numero2 > numero4) {
+        cout<< "el numero mayor es: " << numero2;
+    } else if (numero3 > numero1 && numero3 > numero2 && numero3 > numero4) {
+        cout<< "el numero mayor es: " << numero3;
+    } else {
+        cout<< "el numero mayor es: " << numero4;
+
+    }
+    
+
 
     
-        cout << "El perimetro del circulo es: " << perimetro << endl;
-            cout << "El area del circulo es: " << area << endl;
-
     return 0;
 }
