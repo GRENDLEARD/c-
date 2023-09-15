4bd4fa353d4cceedd9c204ba9f1b062e3ee39405
GRENDLEARD <134238909+GRENDLEARD@users.noreply.github.com>
2023-09-15 11:58:29 -0500

    Actualizar eje1.cpp

--- eje1.cpp
+++ eje1.cpp
@@ -1,25 +1,28 @@
 #include <iostream>
+#include <string>
 
 int main() {
-    double base, resultado = 1;
-    int exponente;
+    // Declara un arreglo para almacenar los números telefónicos de 5 integrantes
+    std::string telefonos[5];
 
-    std::cout << "Ingrese la base: ";
-    std::cin >> base;
-
-    std::cout << "Ingrese el exponente (entero positivo): ";
-    std::cin >> exponente;
-
-    if (exponente < 0) {
-        std::cout << "El exponente debe ser un entero positivo." << std::endl;
-        return 1; // Salir del programa con un código de error
+    // Solicita los números telefónicos y los almacena en el arreglo
+    for (int i = 0; i < 5; i++) {
+        std::cout << "Introduce el número telefónico del integrante " << (i + 1) << ": ";
+        std::cin >> telefonos[i];
     }
 
-    for (int i = 0; i < exponente; i++) {
-        resultado *= base; // Multiplicar la base por sí misma 'exponente' veces
-    }
+    // Pide al usuario que elija un número de integrante para buscar
+    int numeroIntegrante;
+    std::cout << "Ingresa el número de integrante que deseas buscar (1-5): ";
+    std::cin >> numeroIntegrante;
 
-    std::cout << "El resultado de " << base << " elevado a la " << exponente << " es: " << resultado << std::endl;
+    // Verifica que el número de integrante esté en el rango válido
+    if (numeroIntegrante >= 1 && numeroIntegrante <= 5) {
+        // Imprime el número telefónico del integrante seleccionado
+        std::cout << "Número telefónico del integrante " << numeroIntegrante << ": " << telefonos[numeroIntegrante - 1] << std::endl;
+    } else {
+        std::cout << "Número de integrante no válido. Debe estar en el rango 1-5." << std::endl;
+    }
 
     return 0;
 }
